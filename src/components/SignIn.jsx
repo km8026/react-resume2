0a5e31355bd75a735476570d94d41529e3359284
MinJunKim <167392999+km8026@users.noreply.github.com>
2024-07-25 15:13:32 +0900

    asd

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -5,7 +5,7 @@ const SignIn = () => {
   const navigate = useNavigate();
   const [email, setEmail] = useState('');
   const [pwd, setPwd] = useState('');
-  const s = () =>{
+  const s = () => {
     navigate('/2')
   }
   return (
@@ -16,50 +16,52 @@ const SignIn = () => {
         }}>
           <div className="mb-3 input-group flex-nowrap">
             <span className="input-group-text">💻</span>
-            <input 
-            value={email} 
-            onChange={(e) => {
-              setEmail(e.target.value)
-            }}
-            type="text" className="form-control" name="email" placeholder="email" />
+            <input
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value)
+                console.log(email);
+              }}
+              type="text" className="form-control" name="email" placeholder="email" />
           </div>
           <div className="mb-3 input-group flex-nowrap">
             <span className="input-group-text">🔒</span>
-            <input 
-            value={pwd} 
-            onChange={(e) => {
-              setPwd(e.target.value)
-            }}
-            type="password" className="form-control" name="pwd" placeholder="password" />
+            <input
+              value={pwd}
+              onChange={(e) => {
+                setPwd(e.target.value)
+                console.log(pwd);
+              }}
+              type="password" className="form-control" name="pwd" placeholder="password" />
           </div>
           <div className="d-grid gap-2">
-            <button 
-            onClick={() => {
-              async function send() {
-                const url ='http://127.0.0.1:8080/signin';
-                const res = await fetch(url, {
-                  method: 'post',
-                  headers: {
-                    'content-type': 'application/json'
-                  },
-                  body: JSON.stringify( {email: email, password: pwd}) 
-                }); 
-                const data = await res.json();
-              if(data.code === 200) {
-                alert(data.msg);
-                navigate('/');
-                sessionStorage.setItem("jwt", data.result);
-              }else{
-                alert(data.msg);
-              }
-            }
-            send();
+            <button
+              onClick={() => {
+                async function send() {
+                  const url = 'http://127.0.0.1:8080/signin';
+                  const res = await fetch(url, {
+                    method: 'post',
+                    headers: {
+                      'content-type': 'application/json'
+                    },
+                    body: JSON.stringify({ email: email, password: pwd })
+                  });
+                  const data = await res.json();
+                  if (data.code === 200) {
+                    alert(data.msg);
+                    navigate('/');
+                    sessionStorage.setItem("jwt", data.result);
+                  } else {
+                    alert(data.msg);
+                  }
+                }
+                send();
 
-          }}
-            className="btn btn-primary" id="signin">Sign In</button>
+              }}
+              className="btn btn-primary" id="signin">Sign In</button>
           </div>
         </form>
-            <button onClick={s} className="btn btn-primary" id="signup">Sign Up</button>
+        <button onClick={s} className="btn btn-primary" id="signup">Sign Up</button>
       </div>
     </div>
   );
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -3,70 +3,114 @@ import { useNavigate } from "react-router-dom";
 
 const SignUp = () => {
   const navigate = useNavigate();
-  const [email, setEmail] = useState('');
-  const [pwd, setPwd] = useState('');
-  const [name, setName] = useState('');
+  const [email, setEmail] = useState();
+  const [pwd, setPwd] = useState();
+  const [pwdcheck, setPwdcheck] = useState();
+  const [name, setName] = useState();
+  const [emailError, setEmailError] = useState('');
+  const a = pwd === pwdcheck
 
+  const validateEmail = (email) => {
+    const re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return re.test(String(email).toLowerCase());
+  };
+
+  const pwds = (e) => {
+    setPwd(e.target.value);
+  }
+
+  const passwordch = (e) => {
+    setPwdcheck(e.target.value);
+  }
+
+  const emails = (e) => {
+    const emailValue = e.target.value;
+    setEmail(emailValue);
+    if (!validateEmail(emailValue)) {
+      setEmailError('올바른 이메일 형식을 입력하세요.');
+    } else {
+      setEmailError('');
+    }
+  }
   return (
     <div className="container mt-5">
       <div className="row">
-        <form method="post" action="/signup">
+        <form method="post" action="/signup" onSubmit={(e) => {
+          e.preventDefault();
+        }}>
+
           <div className="mb-3">
-            <label for="email">Email:</label>
+            <label htmlFor="email">Email: </label>
             <input
-            value={email} 
-            onChange={(e) => {
-              setEmail(e.target.value)
-            }}
-            type="text" className="form-control" id="email" name="email" />
+              required
+              value={email}
+              onChange={emails}
+              type="email" className="form-control" id="email" name="email" />
           </div>
+          {emailError && <div className="text-danger">{emailError}</div>}
           <div className="mb-3">
-            <label for="pwd">Password:</label>
-            <input 
-            value={pwd} 
-            onChange={(e) => {
-              setPwd(e.target.value)
-            }}
-            type="password" className="form-control" id="pwd" name="pwd" />
+            <label for="pwd">Password: </label>
+            <input
+              value={pwd}
+              onChange={pwds}
+              type="password" className="form-control" id="pwd" name="pwd" />
           </div>
+
+          <div className="form-group">
+            <label htmlFor="confirmPassword">PwdCheck: </label>
+            <input
+              type="password"
+              id="confirmPassword"
+              name="confirmPassword"
+              required  //여기도 state만들고
+              value={pwdcheck}
+              onChange={passwordch}
+            />{!a &&
+              <label id="pw-match" className="pw-match">비밀번호가 일치해야 됩니다.</label>
+
+            }
+          </div>
+
           <div className="mb-3">
-            <label for="name">Name:</label>
-            <input 
-            value={name} 
-            onChange={(e) => {
-              setName(e.target.value)
-            }}
-            type="text" className="form-control" id="name" name="name" />
+            <label for="name">Name: </label>
+            <input
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value)
+              }}
+              type="text" className="form-control" id="name" name="name" />
           </div>
           <div className="d-grid gap-2">
             <button
-             onClick={() => {
-              async function send() {
-                const url ='http://127.0.0.1:8080/signup';
-                const res = await fetch(url, {
-                  method: 'post',
-                  headers: {
-                    'content-type': 'application/json'
-                    
-                    
-                  },
-                    
-                  body: JSON.stringify( {email: email, password: pwd, name: name}) 
-                }); 
-                const data = await res.json();
-              if(data.code === 200) {
-                alert(data.msg);
-                navigate('/');
-              }else{
-                alert('complete');
-              }
-            }
-            send();
+              onClick={() => {
+                async function send() {
+                  const url = 'http://127.0.0.1:8080/signup';
+                  const res = await fetch(url, {
+                    method: 'post',
+                    headers: {
+                      'content-type': 'application/json'
 
-            navigate('/');
-          }}
-            className="btn btn-primary" id="signup">Sign Up</button>
+
+                    },
+
+                    body: JSON.stringify({ email: email, password: pwd, name: name })
+                  });
+                  const data = await res.json();
+                  if (data.code === 200) {
+                    alert(data.msg);
+                    navigate('/1');
+                  } else {
+                    alert('complete');
+                  }
+                }
+
+                send()
+
+              }
+              }
+              className="btn btn-primary" id="signup">Sign Up</button>
           </div>
+
         </form>
       </div>
     </div>
